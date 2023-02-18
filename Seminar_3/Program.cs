483d79d5fc039c5a2488aaf8b81dde0f6f33c216
Mr Alex <alex_konyakhin@mail.ru>
2023-02-18 13:28:57 +0300

    Task_1

---
+++ Seminar_3/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задание1. Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+int FindQuart (int x, int y)
+{
+    if (x > 0 && y > 0) 
+    {
+        return  1;
+    }
+    if (x < 0 && y > 0) return  2;
+    if (x < 0 && y < 0) return  3;
+    if (x > 0 && y < 0) return  4; 
+    return 0;
+}
+Console.WriteLine("Inrut X:" );
+int X = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Inrut Y:" );
+int Y = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(FindQuart(X, Y));
+
+
