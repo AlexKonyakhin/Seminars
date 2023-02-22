f7f7ef8fc5c8788db3f09fdd4f1d3298e4ef9967
Mr Alex <alex_konyakhin@mail.ru>
2023-02-22 22:58:40 +0300

    seminar_4

--- Homework_Seminar_4/Program.cs
+++ Homework_Seminar_4/Program.cs
@@ -10,14 +10,45 @@
 //     int sum = numA * numA;
 //     while (counter <= numB)
 //     {
-//         Console.WriteLine($"{numA} , {numB} -> {Math.Pow(numA, numB)} ");
 //     counter++;
-//     break;
 //     }
+//     Console.WriteLine($"{numA} , {numB} -> {Math.Pow(numA, numB)} ");
 // }
 // Console.WriteLine("Input first number: ");
 // int n = Convert.ToInt32(Console.ReadLine());
 // Console.WriteLine("Input second number: ");
 // int m = Convert.ToInt32(Console.ReadLine());
 // Involution(n, m);
+
+
+// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+// void AllSum(int n)
+// {
+//     int number = n;
+//     int sum = 0;
+//     while (n > 0)
+//     {
+//         int remainder = n % 10;
+//         sum = sum + remainder;
+//         n = n / 10;
+//     }
+//     Console.WriteLine($"{number} -> {sum} ");
+// }
+// Console.WriteLine("Input Number: ");
+// int q = Convert.ToInt32(Console.ReadLine());
+// AllSum(q);
+
+
+//  Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
 
